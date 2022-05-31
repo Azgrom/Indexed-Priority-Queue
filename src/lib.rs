7a84d3e028cbaf781d9efd3db477aa21e2170ecf
Rafael Lúcio <rafaellucio@pm.me>
2022-05-31 20:09:09 -0300

    feat: Implement insert mipq method

--- src/lib.rs
+++ src/lib.rs
@@ -151,25 +151,25 @@ impl<T> MinIndexedPriorityQueue<T>
 where
     T: Clone + PartialOrd,
 {
-    fn new(values: &Vec<T>) -> Self {
-        let mut min_priority_queue = Self {
+    fn new_min_ipq(values: &Vec<T>) -> Self {
+        let mut ipq = Self {
             vals: Self::set_vals(values),
             // position_map:
             // inverse_map:
         };
-        let node_index = min_priority_queue.next_value_index();
+        let node_index = ipq.next_value_index();
         let edge_layer_index_range = Range {
-            start: (min_priority_queue.vals.len() / 2) - 1,
+            start: (ipq.vals.len() / 2) - 1,
             end: node_index,
         };
 
-        edge_layer_index_range.for_each(|i| min_priority_queue.fix_branch_heap_invariant(i));
-        // min_priority_queue.fix_branch_heap_invariant(node_index);
+        edge_layer_index_range
+            .for_each(|edge_index| ipq.fix_min_ipq_branch_heap_invariant(edge_index));
 
-        min_priority_queue
+        ipq
     }
 
-    fn fix_branch_heap_invariant(&mut self, edge_node_index: usize) {
+    fn fix_min_ipq_branch_heap_invariant(&mut self, edge_node_index: usize) {
         let mut ni = edge_node_index;
         let mut pni = parent_node_index(edge_node_index);
 
@@ -194,23 +194,22 @@ where
 
     fn check_potential_min_heap_invariance_integrity_breach(
         &self,
-        value_to_insert: Option<T>,
+        value_to_insert: Option<&T>,
     ) -> bool {
         let next_value_index = self.next_value_index() + 1;
         let pni = parent_node_index(next_value_index);
 
-        !IndexedPriorityQueue::max_priority(&value_to_insert, &self.vals[pni])
+        !IndexedPriorityQueue::max_priority(&Some(value_to_insert.unwrap().clone()), &self.vals[pni])
     }
 
-    fn check_branch_min_heap_invariance_breach(&mut self, mut node_index: usize) {
-        let mut pni = parent_node_index(node_index);
-        let mut invariance_breach = false;
-        while pni != 0 && !invariance_breach {
-            // self.fix_heap_invariant(node_index, pni);
-            invariance_breach =
-                IndexedPriorityQueue::max_priority(&self.vals[node_index], &self.vals[pni]);
-            node_index = pni;
-            pni = parent_node_index(node_index);
+    fn insert(&mut self, value: T) {
+        let does_it_break_heap_invariance = self.check_potential_min_heap_invariance_integrity_breach(Some(&value));
+
+        let nvi = self.next_value_index();
+        self.vals[nvi] = Some(value);
+
+        if does_it_break_heap_invariance {
+            self.fix_min_ipq_branch_heap_invariant(nvi);
         }
     }
 }
@@ -222,9 +221,9 @@ mod min_indexed_pq_tests {
     #[test]
     fn check_min_heap_invariance_integrity_breach() {
         let values = vec![9, 8, 7, 6, 5, 1, 2, 2, 2, 3, 4, 0];
-        let mut min_ipq = MinIndexedPriorityQueue::new(&values);
+        let mut min_ipq = MinIndexedPriorityQueue::new_min_ipq(&values);
 
-        let value_to_insert = Some(8);
+        let value_to_insert = Some(&8);
         let breached_heap_invariance =
             min_ipq.check_potential_min_heap_invariance_integrity_breach(value_to_insert);
 
@@ -232,9 +231,9 @@ mod min_indexed_pq_tests {
     }
 
     #[test]
-    fn test() {
+    fn min_indexed_pq_should_sort_binary_heap_correctly() {
         let values = vec![9, 8, 7, 6, 5, 1, 2, 2, 2, 3, 4, 0];
-        let mut min_ipq = MinIndexedPriorityQueue::new(&values);
+        let mut min_ipq = MinIndexedPriorityQueue::new_min_ipq(&values);
 
         assert_eq!(
             min_ipq.vals,
@@ -258,6 +257,36 @@ mod min_indexed_pq_tests {
             ]
         );
     }
+
+    #[test]
+    fn min_ipq_should_reorder_heap_prioritizing_minimum_value() {
+        let values = vec![9, 8, 7, 6, 5, 1, 2, 2, 2, 3, 4, 0];
+        let mut min_ipq = MinIndexedPriorityQueue::new_min_ipq(&values);
+        let to_insert_value = -1;
+        min_ipq.insert(to_insert_value);
+
+        assert_eq!(
+            min_ipq.vals,
+            [
+                Some(-1),
+                Some(2),
+                Some(0),
+                Some(6),
+                Some(3),
+                Some(1),
+                Some(2),
+                Some(9),
+                Some(8),
+                Some(5),
+                Some(4),
+                Some(7),
+                Some(2),
+                None,
+                None,
+                None
+            ]
+        );
+    }
 }
 
 type MaxIndexedPriorityQueue<T> = IndexedPriorityQueue<T>;
@@ -266,6 +295,47 @@ impl<T> MaxIndexedPriorityQueue<T>
 where
     T: Clone + PartialOrd,
 {
+    fn new_max_ipq(values: &Vec<T>) -> Self {
+        let mut ipq = Self {
+            vals: Self::set_vals(values),
+            // position_map:
+            // inverse_map:
+        };
+        let node_index = ipq.next_value_index();
+        let edge_layer_index_range = Range {
+            start: (ipq.vals.len() / 2) - 1,
+            end: node_index,
+        };
+
+        edge_layer_index_range
+            .for_each(|edge_index| ipq.fix_max_ipq_branch_heap_invariant(edge_index));
+
+        ipq
+    }
+
+    fn fix_max_ipq_branch_heap_invariant(&mut self, edge_node_index: usize) {
+        let mut ni = edge_node_index;
+        let mut pni = parent_node_index(edge_node_index);
+
+        // maximum heap invariance breach
+        let mut mib = |a: &Option<T>, b: &Option<T>| !IndexedPriorityQueue::max_priority(a, b);
+
+        Range {
+            start: 0,
+            end: self.number_of_layers(),
+        }
+        .for_each(|_| {
+            while mib(&self.vals[pni], &self.vals[ni]) && ni != pni {
+                self.vals.swap(pni, ni);
+                ni = pni;
+                pni = parent_node_index(ni);
+            }
+
+            ni = edge_node_index;
+            pni = parent_node_index(edge_node_index);
+        });
+    }
+
     fn check_potential_push_max_heap_invariance_integrity_breach(
         &self,
         value_to_insert: &Option<T>,
@@ -284,12 +354,40 @@ mod max_indexed_pq_tests {
     #[test]
     fn check_max_heap_invariance_integrity_breach() {
         let values = vec![9, 8, 7, 6, 5, 1, 2, 2, 2, 3, 4, 0];
-        let mut ipq = MaxIndexedPriorityQueue::new(&values);
+        let mut ipq = MaxIndexedPriorityQueue::new_max_ipq(&values);
 
         let value_to_insert = Some(8);
         let breached_heap_invariance =
             ipq.check_potential_push_max_heap_invariance_integrity_breach(&value_to_insert);
 
         assert!(breached_heap_invariance);
+    }
+
+    #[test]
+    fn test() {
+        let values = vec![0, 2, 1, 6, 3, 2, 2, 9, 8, 5, 4, 7];
+        let mut ipq = MaxIndexedPriorityQueue::new_max_ipq(&values);
+
+        assert_eq!(
+            ipq.vals,
+            [
+                Some(9),
+                Some(8),
+                Some(7),
+                Some(6),
+                Some(5),
+                Some(2),
+                Some(2),
+                Some(0),
+                Some(2),
+                Some(3),
+                Some(4),
+                Some(1),
+                None,
+                None,
+                None,
+                None
+            ]
+        );
     }
 }
